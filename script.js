e06a21e0fa2520f0438e7dcee3cc7aa83ea6e887
Mark Wilde <markwilde29@gmail.com>
2020-04-21 14:07:18 +0100

    Created a horse constructor object and then some horse objects. Created a method to use on the new horse objects and stored it in the Horse prototype.

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ mary.calculateAge();
 diane.calculateAge();
 
 console.log(john, mary, diane);
+
+var Horse = function (name, carrotsEaten, personality) {
+   this.name = name;
+   this.carrotsEaten = carrotsEaten;
+   this.personality = personality;
+}
+
+Horse.prototype.carrotsAvailable = function () {
+   console.log(21 - this.carrotsEaten);
+}
+
+var ned = new Horse('Ned', 10, 'gregarious')
+var zed = new Horse('Zed', 12, 'gregarious')
+var sweetie = new Horse('Sweetie', 15, 'gregarious')
+
+console.log(ned, zed, sweetie);
+
+ned.carrotsAvailable();
+zed.carrotsAvailable();
+sweetie.carrotsAvailable();
