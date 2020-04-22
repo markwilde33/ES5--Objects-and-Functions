2e1de3dbafaab22ed11ccb7b083fa7bed66d16bc
Mark Wilde <markwilde29@gmail.com>
2020-04-22 18:53:07 +0100

    Gave an example of a function returning other functions.

--- script.js
+++ script.js
@@ -104,38 +104,65 @@
 /////////////////////////////
 // Lecture: Passing functions as arguments
 
-var years = [1990, 1965, 1937, 2005, 1998];
-function arrayCalc(arr, fn) {
-  var arrRes = [];
-  for (var i = 0; i < arr.length; i++) {
-    arrRes.push(fn(arr[i]));
-  }
-  return arrRes;
-}
-function calculateAge(el) {
-  return 2016 - el;
-}
-function isFullAge(el) {
-  return el >= 18;
-}
-function maxHeartRate(el) {
-  if (el >= 18 && el <= 81) {
-    return Math.round(206.9 - 0.67 * el);
+// var years = [1990, 1965, 1937, 2005, 1998];
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+// function calculateAge(el) {
+//   return 2016 - el;
+// }
+// function isFullAge(el) {
+//   return el >= 18;
+// }
+// function maxHeartRate(el) {
+//   if (el >= 18 && el <= 81) {
+//     return Math.round(206.9 - 0.67 * el);
+//   } else {
+//     return -1;
+//   }
+// }
+// function isRetired(el) {
+//   if (el >= 67) {
+//     return "You are drawing the pension!";
+//   } else {
+//     return 67 - el + " years left until retirement";
+//   }
+// }
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
+// var rates = arrayCalc(ages, maxHeartRate);
+// var pension = arrayCalc(ages, isRetired);
+// console.log(pension);
+// console.log(ages);
+// console.log(rates);
+
+/////////////////////////////
+// Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === "designer") {
+    return function (name) {
+      console.log(name + ", can you please explain what UX design is?");
+    };
+  } else if (job === "teacher") {
+    return function (name) {
+      console.log("What subject do you teach, " + name + "?");
+    };
   } else {
-    return -1;
+    return function (name) {
+      console.log("Hello " + name + ", what do you do?");
+    };
   }
 }
-function isRetired(el) {
-  if (el >= 67) {
-    return "You are drawing the pension!";
-  } else {
-    return 67 - el + " years left until retirement";
-  }
-}
-var ages = arrayCalc(years, calculateAge);
-var fullAges = arrayCalc(ages, isFullAge);
-var rates = arrayCalc(ages, maxHeartRate);
-var pension = arrayCalc(ages, isRetired);
-console.log(pension);
-console.log(ages);
-console.log(rates);
+var teacherQuestion = interviewQuestion("teacher");
+var designerQuestion = interviewQuestion("designer");
+teacherQuestion("John");
+designerQuestion("John");
+designerQuestion("jane");
+designerQuestion("Mark");
+designerQuestion("Mike");
+interviewQuestion("teacher")("Mark");
