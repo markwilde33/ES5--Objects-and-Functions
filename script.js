edd7cd969a0109400e584d4275dca8d88e8d17aa
Mark Wilde <markwilde29@gmail.com>
2020-04-25 12:34:51 +0100

    Gave an example of using the bind method in a realistic use case.

--- script.js
+++ script.js
@@ -273,3 +273,23 @@ var emilyFormal = john.presentation.bind(emily, "formal");
 
 emilyFormal("morning");
 emilyFormal("night");
+
+// Another cool example
+var years = [1990, 1965, 1937, 2005, 1998];
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+function calculateAge(el) {
+  return 2016 - el;
+}
+function isFullAge(limit, el) {
+  return el >= limit;
+}
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
