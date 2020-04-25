d6ec552113a38eb9880698dc9f125a491be7604e
Mark Wilde <markwilde29@gmail.com>
2020-04-25 12:16:06 +0100

    Gave examples of how the call, apply and bind methods can be used in JS.

--- script.js
+++ script.js
@@ -188,30 +188,88 @@
 /////////////////////////////
 // Lecture: Closures
 
-function retirement(retirementAge) {
-  var a = " years left until retirement.";
-  return function (yearOfBirth) {
-    var age = 2016 - yearOfBirth;
-    console.log(retirementAge - age + a);
-  };
-}
-var retirementUS = retirement(66);
-var retirementGermany = retirement(65);
-var retirementIceland = retirement(67);
-retirementGermany(1990);
-retirementUS(1990);
-retirementIceland(1990);
-//retirement(66)(1990);
+// function retirement(retirementAge) {
+//   var a = " years left until retirement.";
+//   return function (yearOfBirth) {
+//     var age = 2016 - yearOfBirth;
+//     console.log(retirementAge - age + a);
+//   };
+// }
+// var retirementUS = retirement(66);
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+// retirementGermany(1990);
+// retirementUS(1990);
+// retirementIceland(1990);
+// //retirement(66)(1990);
 
-function interviewQuestion(job) {
-  return function (name) {
-    if (job === "designer") {
-      console.log(name + ", can you please explain what UX design is?");
-    } else if (job === "teacher") {
-      console.log("What subject do you teach, " + name + "?");
-    } else {
-      console.log("Hello " + name + ", what do you do?");
+// function interviewQuestion(job) {
+//   return function (name) {
+//     if (job === "designer") {
+//       console.log(name + ", can you please explain what UX design is?");
+//     } else if (job === "teacher") {
+//       console.log("What subject do you teach, " + name + "?");
+//     } else {
+//       console.log("Hello " + name + ", what do you do?");
+//     }
+//   };
+// }
+// interviewQuestion("teacher")("John");
+
+/////////////////////////////
+// Lecture: Bind, call and apply
+
+var john = {
+  name: "John",
+  age: 26,
+  job: "teacher",
+  presentation: function (style, timeOfDay) {
+    if (style === "formal") {
+      console.log(
+        "Good " +
+          timeOfDay +
+          ", Ladies and gentlemen! I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old."
+      );
+    } else if (style === "friendly") {
+      console.log(
+        "Hey! What's up? I'm " +
+          this.name +
+          ", I'm a " +
+          this.job +
+          " and I'm " +
+          this.age +
+          " years old. Have a nice " +
+          timeOfDay +
+          "."
+      );
     }
-  };
-}
-interviewQuestion("teacher")("John");
+  },
+};
+
+var emily = {
+  name: "Emily",
+  age: 35,
+  job: "designer",
+};
+
+john.presentation("formal", "morning");
+
+john.presentation.call(emily, "friendly", "afternoon");
+
+john.presentation.apply(emily, ["friendly", "afternoon"]);
+
+var johnFriendly = john.presentation.bind(john, "friendly");
+
+johnFriendly("morning");
+johnFriendly("night");
+
+var emilyFormal = john.presentation.bind(emily, "formal");
+
+emilyFormal("morning");
+emilyFormal("night");
